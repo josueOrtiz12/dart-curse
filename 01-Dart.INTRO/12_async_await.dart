204286362a751495855931e5f98f45e03c1ed2c5
josue ortiz <ortizjd0036@gmail.com>
2024-01-10 17:57:17 -0400

    add 12.try_catch class

--- 01-Dart.INTRO/12_async_await.dart
+++ 01-Dart.INTRO/12_async_await.dart
@@ -1,23 +1,21 @@
-void main() async{
-  
+void main() async {
   print('Inicio del programa');
-  
-  try{
-    
+
+  try {
     final value = await httpGet('htttp://fernando-herrera.com/curso');
-    print(value);
-    
-  }catch(err) {
+    print('exito: $value');
+  } catch (err) {
     print('Tenemos un error: ${err}');
+  } finally {
+    print('Fin del try cath');
   }
-  
+
   print('Fin del programa');
 }
 
-Future<String> httpGet( String url) async {
-  
- await Future.delayed(const Duration(seconds : 1));
-  
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
+
   throw 'Erro en la peticion';
 //  return 'Tenemos un valor de la peticion';
 }
